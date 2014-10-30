20c1eb8fb392508951ad3227f1588c9a93bff8c2
Haden <HadenHiles@gmail.com>
2014-10-29 21:11:45 -0400

    -got runs and outs calculating and displaying properly -added new images to represent ground roll doubles and pop flies

--- js/slots.js
+++ js/slots.js
@@ -26,7 +26,7 @@ var doubles = 0;
 var triples = 0;
 var walks = 0;
 var homeRuns = 0;
-var grandSlams = 0;
+var groundRollDoubles = 0;
 var runs = 0;
 var outs = 0;
 
@@ -40,18 +40,20 @@ function init(){
         {src:  "assets/images/slotBg.png", id: "slotBg"},
         {src: "assets/images/largeTextBox.png", id: "moneyTextBox"},
         {src: "assets/images/smallTextBox.png", id: "runsBox"},
-        {src: "assets/images/smallTextBox.png", id: "smallTextBoxOuts"},
+        {src: "assets/images/smallTextBox.png", id: "outsBox"},
         {src: "assets/images/pitchButton.png", id: "pitchButton"},
         {src: "assets/images/resetButton.png", id: "resetGame"},
         {src: "assets/images/bet-up.png", id: "betUp"},
         {src: "assets/images/bet-down.png", id: "betDown"},
-        {src: "assets/images/smallTextBox.png", id: "smallTextBoxBet"},
+        {src: "assets/images/smallTextBox.png", id: "betBox"},
         {src: "assets/images/blank-m.png", id: "blank"},
         {src: "assets/images/strike.png", id: "strike"},
         {src: "assets/images/homerun.png", id: "homerun"},
         {src: "assets/images/single.png", id: "single"},
         {src: "assets/images/double.png", id: "double"},
-        {src: "assets/images/triple.png", id: "triple"}
+        {src: "assets/images/triple.png", id: "triple"},
+        {src: "assets/images/popfly.png", id: "popfly"},
+        {src: "assets/images/groundrolldouble.png", id: "groundrolldouble"}
     ]);
 }
 
@@ -68,15 +70,17 @@ function handleComplete(event){
     var moneyLabel = new createjs.Text("Money", "bold 18px Arial", "#fff");
     var money = new createjs.Text(playerMoney, "bold 34px Arial", "#fff");
     var runsBox = new createjs.Bitmap(queue.getResult("runsBox"));
-    var smallTextBoxOuts = new createjs.Bitmap(queue.getResult("smallTextBoxOuts"));
+    var runsLabel = new createjs.Text("Runs", "bold 18px Arial", "#fff");
+    var runsNumber = new createjs.Text(runs, "bold 34px Arial", "#fff");
+    var outsBox = new createjs.Bitmap(queue.getResult("outsBox"));
     var outsLabel = new createjs.Text("Outs", "bold 18px Arial", "#fff");
+    var outsNumber = new createjs.Text(outs, "bold 34px Arial", "#fff");
     var pitchButton = new createjs.Bitmap(queue.getResult("pitchButton"));
     var resetGame = new createjs.Bitmap(queue.getResult('resetGame'));
     var betUp = new createjs.Bitmap(queue.getResult("betUp"));
     var betDown = new createjs.Bitmap(queue.getResult("betDown"));
-    var smallTextBoxBet = new createjs.Bitmap(queue.getResult("smallTextBoxBet"));
+    var betBox = new createjs.Bitmap(queue.getResult("betBox"));
     var playerBetAmount = new createjs.Text("Bet", "bold 34px Arial", "#fff");
-    var runsLabel = new createjs.Text("Runs", "bold 18px Arial", "#fff");
     var playerStats = new createjs.Text("Jackpot: " + jackpot
         + "\nPitches: " + pitches
         + "\nWins: " + winNumber
@@ -103,10 +107,14 @@ function handleComplete(event){
     runsBox.y += 45;
     runsLabel.x += 96;
     runsLabel.y += 22;
-    smallTextBoxOuts.x += 320;
-    smallTextBoxOuts.y += 45;
+    runsNumber.x += 91;
+    runsNumber.y += 46;
+    outsBox.x += 320;
+    outsBox.y += 45;
     outsLabel.x += 335;
     outsLabel.y += 22;
+    outsNumber.x += 326;
+    outsNumber.y += 45;
     pitchButton.x += 325;
     pitchButton.y += 305;
     resetGame.x += 280;
@@ -115,8 +123,8 @@ function handleComplete(event){
     betUp.y += 305;
     betDown.x += 122;
     betDown.y += 305;
-    smallTextBoxBet.x += 165;
-    smallTextBoxBet.y += 305;
+    betBox.x += 165;
+    betBox.y += 305;
     playerBetAmount.x += 171;
     playerBetAmount.y += 306;
     playerStats.x += 410;
@@ -133,13 +141,15 @@ function handleComplete(event){
     stage.addChild(money);
     stage.addChild(runsBox);
     stage.addChild(runsLabel);
-    stage.addChild(smallTextBoxOuts);
+    stage.addChild(runsNumber);
+    stage.addChild(outsBox);
     stage.addChild(outsLabel);
+    stage.addChild(outsNumber);
     stage.addChild(pitchButton);
     stage.addChild(resetGame);
     stage.addChild(betUp);
     stage.addChild(betDown);
-    stage.addChild(smallTextBoxBet);
+    stage.addChild(betBox);
     stage.addChild(playerBetAmount);
     stage.addChild(playerStats);
 
@@ -174,11 +184,11 @@ function handleComplete(event){
     }
     /* When the player clicks the pitch button the game starts */
     function pitch() {
-        if (playerMoney == 0)
+        if (playerMoney == 0 || outs == 3)
         {
             if (confirm("Game over! \nDo you want to play again?")) {
                 resetAll();
-                showPlayerStats();
+                updatePlayerStats();
             }
         }
         else if (playerBet > playerMoney) {
@@ -216,7 +226,7 @@ function handleComplete(event){
 
             determineWinnings();
             pitches++;
-            showPlayerStats();
+            updatePlayerStats();
         }
         else {
             alert("Please enter a valid bet amount");
@@ -224,9 +234,74 @@ function handleComplete(event){
     }
 
     /* Utility function to show Player Stats */
-    function showPlayerStats()
+    function updatePlayerStats()
     {
         var winRatio = (winNumber/pitches * 100).toFixed(2);
+
+        //update the player stats
+        stage.removeChild(playerStats);
+        playerStats = new createjs.Text("Jackpot: " + jackpot
+            + "\nPitches: " + pitches
+            + "\nWins: " + winNumber
+            + "\nLosses: " + lossNumber
+            + "\nWin Ratio: " + winRatio + "%"
+            , "bold 18px Arial", "#fff");
+        playerStats.x += 410;
+        playerStats.y += 85;
+        stage.addChild(playerStats);
+
+        //update the player money text
+        stage.removeChild(money);
+        money = new createjs.Text(playerMoney, "bold 34px Arial", "#fff");
+        money.x += 188;
+        money.y += 46;
+        stage.addChild(money);
+
+        //update the number of runs
+        stage.removeChild(runsNumber);
+        runsNumber = new createjs.Text(runs, "bold 34px Arial", "#fff");
+        runsNumber.x += 91;
+        runsNumber.y += 45;
+        stage.addChild(runsNumber);
+
+        //update the number of outs
+        stage.removeChild(outsNumber);
+        outsNumber = new createjs.Text(outs, "bold 34px Arial", "#fff");
+        outsNumber.x += 326;
+        outsNumber.y += 45;
+        stage.addChild(outsNumber);
+    }
+
+    /* Utility function to reset all fruit tallies */
+    function resetFruitTally() {
+        fouls = 0;
+        singles = 0;
+        doubles = 0;
+        triples = 0;
+        popFlies = 0;
+        walks = 0;
+        homeRuns = 0;
+        strikes = 0;
+    }
+
+    /* Utility function to reset the player stats */
+    function resetAll() {
+        playerMoney = 1000;
+        winnings = 0;
+        jackpot = 5000;
+        pitches = 0;
+        playerBet = "Bet";
+        winNumber = 0;
+        lossNumber = 0;
+        winRatio = 0;
+        runs = 0;
+        outs = 0;
+
+        stage.removeChild(playerBetAmount);
+        playerBetAmount = new createjs.Text(playerBet, "bold 34px Arial", "#fff");
+        playerBetAmount.x += 171;
+        playerBetAmount.y += 306;
+        stage.addChild(playerBetAmount);
 
         stage.removeChild(playerStats);
         playerStats = new createjs.Text("Jackpot: " + jackpot
@@ -244,80 +319,44 @@ function handleComplete(event){
         money.x += 188;
         money.y += 46;
         stage.addChild(money);
-        return playerStats;
-    }
 
-    /* Utility function to reset all fruit tallies */
-    function resetFruitTally() {
-        fouls = 0;
-        singles = 0;
-        doubles = 0;
-        triples = 0;
-        popFlies = 0;
-        walks = 0;
-        homeRuns = 0;
-        strikes = 0;
-    }
+        //update the number of runs
+        stage.removeChild(runsNumber);
+        runsNumber = new createjs.Text("0", "bold 34px Arial", "#fff");
+        runsNumber.x += 91;
+        runsNumber.y += 45;
+        stage.addChild(runsNumber);
 
-    /* Utility function to reset the player stats */
-    function resetAll() {
-//        if(confirm("Are you sure you want to restart your game?")){
-            playerMoney = 1000;
-            winnings = 0;
-            jackpot = 5000;
-            pitches = 0;
-            playerBet = "Bet";
-            winNumber = 0;
-            lossNumber = 0;
-            winRatio = 0;
+        //update the number of outs
+        stage.removeChild(outsNumber);
+        outsNumber = new createjs.Text("0", "bold 34px Arial", "#fff");
+        outsNumber.x += 326;
+        outsNumber.y += 45;
+        stage.addChild(outsNumber);
 
-            stage.removeChild(playerBetAmount);
-            playerBetAmount = new createjs.Text(playerBet, "bold 34px Arial", "#fff");
-            playerBetAmount.x += 171;
-            playerBetAmount.y += 306;
-            stage.addChild(playerBetAmount);
-
-            stage.removeChild(playerStats);
-            playerStats = new createjs.Text("Jackpot: " + jackpot
-                + "\nPitches: " + pitches
-                + "\nWins: " + winNumber
-                + "\nLosses: " + lossNumber
-                + "\nWin Ratio: " + winRatio + "%"
-                , "bold 18px Arial", "#fff");
-            playerStats.x += 410;
-            playerStats.y += 85;
-            stage.addChild(playerStats);
-
-            stage.removeChild(money);
-            money = new createjs.Text(playerMoney, "bold 34px Arial", "#fff");
-            money.x += 188;
-            money.y += 46;
-            stage.addChild(money);
-
-            //UPDATE THE REEL IMAGES
-            stage.removeChild(reel3);
-            stage.removeChild(reel2);
-            stage.removeChild(reel1);
-            stage.removeChild(slotBg);
-            reel1 = new createjs.Bitmap(queue.getResult("blank"));
-            reel2 = new createjs.Bitmap(queue.getResult("blank"));
-            reel3 = new createjs.Bitmap(queue.getResult("blank"));
-            slotBg = new createjs.Bitmap(queue.getResult("slotBg"));
-            //position bitmaps
-            slotBg.x += 80;
-            slotBg.y += 85;
-            reel3.x += 292;
-            reel3.y += 152;
-            reel2.x += 190;
-            reel2.y += 152;
-            reel1.x += 95;
-            reel1.y += 152;
-            //Display bitmaps
-            stage.addChild(reel3);
-            stage.addChild(reel2);
-            stage.addChild(reel1);
-            stage.addChild(slotBg);
-//        }
+        //UPDATE THE REEL IMAGES
+        stage.removeChild(reel3);
+        stage.removeChild(reel2);
+        stage.removeChild(reel1);
+        stage.removeChild(slotBg);
+        reel1 = new createjs.Bitmap(queue.getResult("blank"));
+        reel2 = new createjs.Bitmap(queue.getResult("blank"));
+        reel3 = new createjs.Bitmap(queue.getResult("blank"));
+        slotBg = new createjs.Bitmap(queue.getResult("slotBg"));
+        //position bitmaps
+        slotBg.x += 80;
+        slotBg.y += 85;
+        reel3.x += 292;
+        reel3.y += 152;
+        reel2.x += 190;
+        reel2.y += 152;
+        reel1.x += 95;
+        reel1.y += 152;
+        //Display bitmaps
+        stage.addChild(reel3);
+        stage.addChild(reel2);
+        stage.addChild(reel1);
+        stage.addChild(slotBg);
     }
 
 
@@ -380,7 +419,7 @@ function handleComplete(event){
                     doubles++;
                     break;
                 case checkRange(outCome[spin], 47, 54): // 12.3% probability
-                    betLine[spin] = "strike";
+                    betLine[spin] = "popfly";
                     popFlies++;
                     break;
                 case checkRange(outCome[spin], 55, 59): //  7.7% probability
@@ -388,12 +427,12 @@ function handleComplete(event){
                     triples++;
                     break;
                 case checkRange(outCome[spin], 60, 62): //  4.6% probability
-                    betLine[spin] = "strike";
+                    betLine[spin] = "popfly";
                     popFlies++;
                     break;
                 case checkRange(outCome[spin], 63, 64): //  3.1% probability
-                    betLine[spin] = "strike";
-                    walks++;
+                    betLine[spin] = "groundrolldouble";
+                    groundRollDoubles++;
                     break;
                 case checkRange(outCome[spin], 65, 65): //  1.5% probability
                     betLine[spin] = "homerun";
@@ -407,60 +446,48 @@ function handleComplete(event){
     /* This function calculates the player's winnings, if any */
     function determineWinnings()
     {
-        if (strikes == 0)
-        {
-            if (singles == 3) {
-                runs++;
-                winnings = playerBet * 10;
-            }
-            else if(doubles == 3) {
-                runs = runs + 2;
-                winnings = playerBet * 20;
-            }
-            else if (triples == 3) {
-                runs = runs + 3;
-                winnings = playerBet * 30;
-            }
-            else if (popFlies == 3) {
-                winnings = playerBet * 40;
-            }
-            else if (walks == 3) {
-                winnings = playerBet * 75;
-            }
-            else if (homeRuns == 3) {
-                runs = runs + 4;
-                winnings = playerBet * 100;
-            }
-            else if (singles == 2) {
-                winnings = playerBet * 2;
-            }
-            else if (doubles == 2) {
-                winnings = playerBet * 2;
-            }
-            else if (triples == 2) {
-                runs = runs + 2;
-                winnings = playerBet * 3;
-            }
-            else if (popFlies == 2) {
-                outs++;
-                winnings = playerBet * 4;
-            }
-            else if (homeRuns == 2) {
-                runs = runs + 3;
-                winnings = playerBet * 20;
-            }
-            else if (homeRuns == 1) {
-                runs = runs + 2;
-                winnings = playerBet * 5;
-            }
-            else {
-                winnings = playerBet * 1;
-            }
+        if (singles == 3) {
+            runs++;
+            winnings = playerBet * 10;
             winNumber++;
             showWinMessage();
         }
-        else
-        {
+        else if(doubles == 3) {
+            runs = runs + 2;
+            winnings = playerBet * 20;
+            winNumber++;
+            showWinMessage();
+        }
+        else if (triples == 3) {
+            runs = runs + 3;
+            winnings = playerBet * 30;
+            winNumber++;
+            showWinMessage();
+        }
+        else if (homeRuns == 3) {
+            runs = runs + 4;
+            winnings = playerBet * 100;
+            winNumber++;
+            showWinMessage();
+        }
+        else if(groundRollDoubles == 3){
+            runs = runs + 2;
+            winnings = playerBet * 20;
+            winNumber++;
+            showWinMessage();
+        }
+        else if (homeRuns == 1) {
+            runs++;
+            winnings = playerBet * 10;
+            winNumber++;
+            showWinMessage();
+        }
+        if(strikes == 3 || popFlies == 3) {
+            outs++;
+            lossNumber++;
+            showLossMessage();
+        }
+        else {
             lossNumber++;
             showLossMessage();
         }
